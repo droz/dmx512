6c9d1406db725818916053a3ea0ab54e2dbb7375
Pierre-Yves Droz <py.droz@gmail.com>
2023-11-29 18:07:09 -0800

    More comments

--- java/DmxExample.java
+++ java/DmxExample.java
@@ -6,10 +6,66 @@ import java.io.IOException;
 import java.net.*;
 
 public class DmxExample {
+    /*
+     * This is the actual useful fun code
+     * 
+     * Each DMX universe has 512 channels.
+     * Each channel is a byte, and it can have different meanings, depending on the device (light, dimmer, gobo, etc...).
+     * For example, a channel can be the brightness of a light, or the position of a moving head.
+     * Usually a device will use a few channels, starting at a given offset.
+     * If you have multiple lights, you just need to fill the corresponding channels in the data array and send
+     * everyting at the same time.
+     * The following example is for a light that uses 11 channels, starting at channel 1 (index 0 in the array).
+     */
+
+    public static void main(String[] args) {
+        // Connect to the server.
+        // The raspberry pi is called "dmxhub.local", and the DMX server is on port 5419
+        //  (the port number is like a secondary address that is used to distinguish different servers on the same machine)
+        if (!connect("dmxhub.local", 5419)) {
+            return;
+        }
+        // Allocate the data array (always 512 bytes, that's why its called "DMX512")
+        byte[] data = new byte[512];
+
+        // Set the first 11 channels to some values
+        data[0]  = (byte) 0;   // Pan
+        data[1]  = (byte) 0;   // Tilt
+        data[2]  = (byte) 0;   // Pan/Tilt speed (0 fastest -> 255 slowest)
+        data[3]  = (byte) 255;  // Global dimmer (0 off -> 255 full brighness)
+        data[4]  = (byte) 0;   // Strobe (0 off, 1 slow -> 255 fast)
+        data[5]  = (byte) 0;   // Red
+        data[6]  = (byte) 0;   // Green
+        data[7]  = (byte) 0;   // Blue
+        data[8]  = (byte) 0;   // White
+        data[9]  = (byte) 0;   // Orange
+        data[10] = (byte) 0;   // UV (black light)
+
+        // Now we loop while sending data to the server, incrementing values everytime to make stuff move.
+        while(true) { 
+            if (!sendData(data)) {
+                return;
+            }
+            data[0]++;
+            data[1]++;
+            data[5]++;
+            data[7]--;
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     /*
      * START of client code
-     * ( No need to understand the details of this )
+     * ( No need to understand the details of this part,
+     *   this is the code that interacts with the server )
      */
 
     // The socket we use to connect to the server
@@ -69,42 +125,4 @@ public class DmxExample {
     /*
      * END of client code
      */
-    
-    /*
-     * Now is the actual useful fun code
-     * 
-     * Each DMX universe has 512 channels. Each channel is a byte,
-     * and it can have different meanings.
-     * Usually a device (light/dimmer/...) will use a few channels, starting at a given channel.
-     * The following example is for a light that uses 11 channels, starting at channel 1 (index 0 in the array).
-     */
-
-    public static void main(String[] args) {
-        if (!connect("dmxhub.local", 5419)) {
-            return;
-        }
-        byte[] data = new byte[512];
-        data[0]  = (byte) 0;   // Pan
-        data[1]  = (byte) 0;   // Tilt
-        data[2]  = (byte) 0;   // Pan/Tilt speed (0 fastest -> 255 slowest)
-        data[3]  = (byte) 255;  // Global dimmer (0 off -> 255 full brighness)
-        data[4]  = (byte) 0;   // Strobe (0 off, 1 slow -> 255 fast)
-        data[5]  = (byte) 0;   // Red
-        data[6]  = (byte) 0;   // Green
-        data[7]  = (byte) 0;   // Blue
-        data[8]  = (byte) 0;   // White
-        data[9]  = (byte) 0;   // Orange
-        data[10] = (byte) 0;   // UV (black light)
-
-        while(true) { 
-            if (!sendData(data)) {
-                return;
-            }
-            data[0]++;
-            data[1]++;
-            data[5]++;
-            data[7]--;
-        }
-    }
-
 }
